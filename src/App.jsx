4f0e2fa041ba7671ab0741e9ff97596c0da496ed
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-05-28 17:18:32 -0300

    se agregan estilos basicos de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Container } from 'react-bootstrap';
 function App() {
 
   return (
     <>
-
+        <Container id='containerPrincipal'>
+        </Container>
+    <footer className='bg-dark text-light text-center py-3'>
+      <p>&copy; Todos los derechos reservados</p>
+    </footer>
     </>
   )
 }
